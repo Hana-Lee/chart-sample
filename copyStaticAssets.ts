9fb98a46a1dbbc7d1c8679a2384c85237ca85942
Hana Lee <i@leehana.co.kr>
2019-04-05 00:34:44 +0900

    누락된 manifest.json 추가

--- copyStaticAssets.ts
+++ copyStaticAssets.ts
@@ -4,3 +4,4 @@ shell.cp('-R', 'src/public/javascripts', 'dist/public/javascripts/');
 shell.cp('-R', 'src/public/fonts', 'dist/public/');
 shell.cp('-R', 'src/public/images', 'dist/public/');
 shell.cp('-R', 'src/data.json', 'dist/data.json');
+shell.cp('-R', 'src/manifest.json', 'dist/manifest.json');
