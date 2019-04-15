07c94b8ce235798f044154f76e6817555b1981a5
Hana Lee <i@leehana.co.kr>
2019-04-15 10:23:17 +0900

    * 불필요한 파일 제거

--- copyStaticAssets.ts
+++
@@ -1,9 +0,0 @@
-import * as shell from 'shelljs';
-
-// shell.cp('-R', 'src/public/javascripts/app.js', 'dist/public/javascripts/app.js');
-shell.cp('-R', 'src/public/javascripts/main.js', 'dist/public/javascripts/main.js');
-// shell.cp('-Rf', 'src/public/javascripts', 'dist/public/javascripts');
-shell.cp('-Rf', 'src/public/fonts', 'dist/public/');
-shell.cp('-Rf', 'src/public/images', 'dist/public/');
-shell.cp('-Rf', 'src/data.json', 'dist/data.json');
-shell.cp('-Rf', 'src/manifest.json', 'dist/public/javascripts/manifest.json');
